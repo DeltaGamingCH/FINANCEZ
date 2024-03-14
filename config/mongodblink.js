f056adf20f1b32a3fb8be831bfe4c3eae8a58bb2
DeltaGamingCH <88480788+DeltaGamingCH@users.noreply.github.com>
2024-03-14 08:43:48 +0100

    Delete config/mongodblink.js

--- config/mongodblink.js
+++
@@ -1,3 +0,0 @@
-const mongodblink = 'mongodb+srv://databaseUser:FinancezDB0804@financezcluster.4fcese5.mongodb.net/?retryWrites=true&w=majority&appName=FinancezCluster';
-
-module.exports = mongodblink;

8f83882e14f70e86b142093a5fb9bff3eb037f54
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-03-14 08:40:05 +0100

    Added link to local

---
+++ config/mongodblink.js
@@ -0,0 +1,3 @@
+const mongodblink = 'mongodb+srv://databaseUser:FinancezDB0804@financezcluster.4fcese5.mongodb.net/?retryWrites=true&w=majority&appName=FinancezCluster';
+
+module.exports = mongodblink;

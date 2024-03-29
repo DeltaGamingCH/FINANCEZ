7e788f2eaaf95a0eb69601593b0b723b91f67bb1
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-03-29 11:33:02 +0100

    Removed debugging code, minor styling changes

--- common/js/sidebarToggle.js
+++ common/js/sidebarToggle.js
@@ -2,7 +2,5 @@ document.querySelectorAll('button').forEach(button => {
     button.addEventListener('click', function() {
         document.querySelector('.sidebar').classList.toggle('sidebar-hidden');
         document.querySelector('button').classList.toggle('sidebar-header-button-toggled');
-        console.log('Print');
-        
     });
 });

0a6751cbacc7ffaeedffa4941726d0cac28cac56
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-03-30 09:17:10 +0100

    Attempt to fix responsive .content

--- common/js/sidebarToggle.js
+++ common/js/sidebarToggle.js
@@ -2,6 +2,6 @@ document.querySelectorAll('button').forEach(button => {
     button.addEventListener('click', function() {
         document.querySelector('.sidebar').classList.toggle('sidebar-hidden');
         document.querySelector('button').classList.toggle('sidebar-header-button-toggled');
-        document.querySelector('.content').classList.toggle('sidebar-hidden');
+        /*document.querySelector('.content').classList.toggle('sidebar-hidden');*/
     });
 });
